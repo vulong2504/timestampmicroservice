e35647dc2c53da51265a9bd030b044278a48b9ed
Glitch (vulong2504-api-project-timestamp-microservice) <none>
2020-03-21 08:57:03 +0000

    ☕️🎢 Checkpoint ./server.js:12995973/2795

--- server.js
+++ server.js
@@ -25,15 +25,27 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/timestamp",(req,res)=>{
-  res.json({unix: Date.now(),utc: new Date(Date.now()).toUTCString()})
+  res.json({unix: Date.now(),utc: Date()})
 })
-app.get('/api/timestamp/:date_string',(req,res)=>{
-  const data=req.param.date_string
-  if(/\d{5,}/.test(data)) {
-    const num=Number(data)
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
   }
 
-})
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({unix: null, utc : "Invalid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
